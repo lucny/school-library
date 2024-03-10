5982dc9c741af9ec8ace509194db7cefd96ad60a
lucny <lucny@sspu-opava.cz>
2024-03-10 17:10:51 +0100

    Datovy model knihovny - zaklad

--- library/models.py
+++ library/models.py
@@ -3,12 +3,41 @@ from django.db import models
 
 # Create your models here.
 
-class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    birth_date = models.DateField()
-    death_date = models.DateField(null=True, blank=True)
-    biography = models.TextField(blank=True)
+class Autor(models.Model):
+    jmeno = models.CharField(max_length=100)
+    prijmeni = models.CharField(max_length=100)
+    narozeni = models.DateField()
+    umrti = models.DateField(blank=True, null=True)
+    zivotopis = models.TextField()
+    fotografie = models.ImageField(upload_to='autori/')
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.jmeno} {self.prijmeni}'
+
+
+class Zanr(models.Model):
+    nazev = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nazev
+
+
+class Vydavatelstvi(models.Model):
+    nazev = models.CharField(max_length=100)
+    adresa = models.TextField()
+
+    def __str__(self):
+        return self.nazev
+
+
+class Kniha(models.Model):
+    titul = models.CharField(max_length=100)
+    obsah = models.TextField()
+    pocet_stran = models.IntegerField()
+    rok_vydani = models.IntegerField()
+    autor = models.ManyToManyField(Autor)
+    zanr = models.ManyToManyField(Zanr)
+    vydavatelstvi = models.ForeignKey(Vydavatelstvi, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titul
